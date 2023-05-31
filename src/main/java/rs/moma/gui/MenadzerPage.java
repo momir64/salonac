0a4a5561f0e20935c22f371a9054f41c391ff610
Momir Stanišić <momir.stanisic.ns@gmail.com>
2023-05-31 07:21:07 +0200

    Dodat menager gui

--- src/main/java/rs/moma/gui/MenadzerPage.java
+++ src/main/java/rs/moma/gui/MenadzerPage.java
@@ -9,13 +9,31 @@ public class MenadzerPage extends JFrame {
     private JPanel      mainPanel;
     private JTabbedPane tabbedPane;
     private JPanel      bazaPanel;
-    private JPanel finansijePanel;
-    private JPanel karticaLojalnostiPanel;
-    private JPanel izvestajiPanel;
-    private JPanel dijagramiPanel;
+    private JPanel      finansijePanel;
+    private JPanel      karticaLojalnostiPanel;
+    private JPanel      izvestajiPanel;
+    private JPanel      dijagramiPanel;
+    private JTable      table1;
+    private JComboBox   comboBox1;
+    private JButton     sacuvajPraviloButton;
+    private JButton     obrisiPraviloButton;
+    private JTextField  textField1;
+    private JComboBox   comboBox2;
+    private JTextField  textField2;
+    private JTextField  textField3;
+    private JButton     prikaziButton;
+    private JList       list1;
+    private JTextField  textField4;
+    private JButton     generisiListuKlijenataSaButton;
+    private JTable      table2;
+    private JComboBox   comboBox3;
+    private JButton     dodajButton;
+    private JButton     izmeniButton;
+    private JButton     obrisiButton;
+    private JTable      table3;
 
     public MenadzerPage(Zaposlen menadzer) {
-        setSize(1020, 1108);
+        setSize(1300, 950);
         setContentPane(mainPanel);
         setLocationRelativeTo(null);
         setMinimumSize(new Dimension(950, 801));
